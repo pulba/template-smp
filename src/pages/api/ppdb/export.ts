a7fb67b2bc859480524eefd203ee90a9bbf4cf69
pulba <pulbagrup@gmail.com>
2026-01-29 01:18:03 +0700

    template smp update

--- src/pages/api/ppdb/export.ts
+++ src/pages/api/ppdb/export.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from 'astro';
 import db from '../../../lib/db';
 
+export const prerender = false;
 export const GET: APIRoute = async ({ url }) => {
   const tahun = url.searchParams.get('tahun');
 
